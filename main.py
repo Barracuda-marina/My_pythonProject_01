d5502f46b7c4a55a9f2babda261774015d53d1b8
Barracuda-marrina <barracudamarrina@gmail.com>
2021-11-18 16:38:17 +0200

    2nd commit

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+
 class Point:
-    "Класс для представления координат точек на плоскости"
+    "Класс для представления аттрибутов точек на плоскости"
     color = 'red'
-    circle = 2
+    sphere = 5
+
+    def setCoord():
+        print('Вызов метода setCoords')
+
+class Coord:
+    "Класс для представления координат точек в 3d"
     x = 1
     y = 1
+    z = 1
 
 class Human:
-    "Класс для описания человека"
+    "Класс для описания параметров человека"
     sex = 'male'
     age = 1.0
     date_of_birth = '01/01/1980'
@@ -19,3 +27,6 @@ if not getattr(Point, 'f', False):
     print("No data")
 else:
     print("Data are present")
+
+a = Point()
+b = Point()
